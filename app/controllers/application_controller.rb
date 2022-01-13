0c2de63ad373879d31314e597e3c6d3c5dac42ed
Guy <guymoustapha@gmail.com>
2022-01-13 19:59:27 +0200

    Write method to calculate weeks

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,7 +4,7 @@ class ApplicationController < ActionController::Base
   def calculate_weeks
     str = '2021-09-01'
 
-    # Date.parse(str).step(Date.today, 7).count                  
+    Date.parse(str).step(Date.today, 7).count                  
     Date.parse(str).upto(Date.today).count.fdiv(7).round(2) 
   end
 end
